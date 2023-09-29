fcbd068e949467f81fdbb5d02140576845238f69
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-09-29 14:42:51 +0500

    complete the secton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,91 @@
-import logo from './logo.svg';
+// import { useState } from 'react';
 import './App.css';
+// import { AiOutlineHeart } from 'react-icons/ai';
+// import { AiFillHeart } from 'react-icons/ai';
 
-function App() {
+
+
+
+const initialItems = [
+  {id: 1, description: "Passports", quantity: 2 , packed: true },
+  {id: 2, description: "Sock", quantity: 12 , packed:false },
+  {id: 3, description: "Charger", quantity: 1 , packed: false },
+
+
+]
+
+export default function App() {
   return (
-    <div className="App">
-      <h1 className='bg-red-300'>hello</h1>
+    <div className='flex items-center justify-center flex-col '>
+     <Logo/>
+     <Form/>
+     <PakingList/>
+     <Stats/>
     </div>
-  );
+  )
 }
 
-export default App;
+
+function Logo (){
+  return(
+  <div className='bg-orange-300 w-full flex items-center justify-center'>
+    <h1 className='text-red-700 text-[60px] uppercase font-extrabold  ' >🏝 Far Away 🧳 </h1>
+  </div>
+  )
+}
+
+function Form (){
+  return(
+  <form className='bg-green-900 w-full h-20 gap-4 flex items-center justify-center'>
+    <h1 className='text-white text-[20px]' >What do you need for your 😍 trip? </h1>
+    <select className='rounded-xl p-3 px-2 '>
+      <option value={1} >1</option>
+      <option value={2} >2</option>
+      <option value={3} >3</option>
+    </select>
+    <input className='rounded-xl p-3' type='text' placeholder='Item...' />
+    <button className='border-spacing-6 border rounded-xl p-3 text-white bg-green-500 '>add</button>
+  </form>
+
+  )
+}
+
+
+function PakingList (){ 
+  return(
+  <div className='bg-yellow-400 w-full h-[535px]  px-20   flex '>
+    <div className='flex w-auto items-center  h-36'>
+    <ul className='text-[30px] flex flex-row justify-center gap-16' >
+   {initialItems.map((item =>(
+   <Item  item={item} />)
+   ))}
+    </ul>
+  </div>
+  </div>
+  )
+}
+
+function Item ({item}){
+  return (
+    <li className='flex gap-4'>
+      <span className='text-[22px]' style={item.packed ? {textDecorationLine: "line-through" } : {} }>
+      {item.quantity}
+      {item.description}
+      </span>
+      <button className='text-[15px] '>❌</button>
+      </li> 
+  )
+}
+
+function Stats (){
+  return(
+  <footer className='bg-green-300 w-full flex items-center justify-center '>
+    <em>
+    <h1 className='text-red-700 text-[20px]' >💼 You have X items on your list, and you already picked X (X%)</h1>
+    </em>
+  </footer>
+  )
+}
+
+
+
