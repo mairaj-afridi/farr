718c8ab83775894324dc69ed272909b2d40e5d77
MAIRAJ AFRIDI <mairajafridi5@gmail.com>
2023-09-30 22:14:18 +0500

    To added the if conditon.if the arrya are empty so desplay this message 🚀

--- src/App.js
+++ src/App.js
@@ -98,14 +98,20 @@ function Item({ item, onDeleteItem, onToggleItems }) {
 }
 
 function Stats({items}) {
+if(!items.length) return (
+  <p className='bg-green-300 py-5 w-full flex items-center justify-center' >
+    <em className='text-white text-[26px]'>Start adding some Items to your packing list 🚀</em>
+    </p>
+)
+
 const numItems = items.length;
 const numPaked = items.filter((item) => item.packed).length;
 const percentage = Math.round((numPaked/numItems) * 100);
 
   return (
-    <footer className='bg-green-300 w-full flex items-center justify-center '>
-      <em>{percentage === 100 ? "You got everything! Ready to go 🛩" : 
-       `💼 You have ${numItems} items on your list, and you already picked ${numPaked} (${percentage}X%)`
+    <footer className='bg-green-300 py-5 w-full flex items-center justify-center '>
+      <em className='text-white text-[26px]'>{percentage === 100 ? "You got everything! Ready to go ✈️" : 
+       `💼 You have ${numItems}  items on your list, and you already picked ${numPaked} (${percentage}X%)`
       }
       </em>
     </footer>
